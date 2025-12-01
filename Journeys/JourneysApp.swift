79cb102af6a24eca026a9ca2ebe98d020e98cd04
Martin <marrty.h.1992@gmail.com>
2025-12-01 13:52:56 +0100

    Initial Commit

---
+++ Journeys/JourneysApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneysApp.swift
+//  Journeys
+//
+//  Created by Martin Hrbáček on 01.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct JourneysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
